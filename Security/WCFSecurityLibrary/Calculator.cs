23773c4a423e2091866b2778868dfb2e773037c0
Umberto <Umbertogotti1979@gmail.com>
2019-01-21 08:31:17 +0100

    Security project added.

---
+++ Security/WCFSecurityLibrary/Calculator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Security.Permissions;
+using System.Security.Principal;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCFSecurityLibrary
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class Calculator : ICalculator
+    {
+        public int Add(int a, int b)
+        {
+            try
+            {
+                return a + b;
+            }
+            catch (Exception)
+            {
+                throw new FaultException("Access denied");
+            }
+
+        }
+
+        public int Multiply(int a, int b)
+        {
+            try
+            {
+                return a * b;
+            }
+            catch (Exception)
+            {
+                throw new FaultException("Access denied");
+            }
+        }
+    }
+}
